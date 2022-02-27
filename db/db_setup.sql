8145ae0f11b6a0b493dd40083238f6ef7a32425f
vvpanchev <vladislav_3@abv.bg>
2022-02-27 19:45:22 +0200

    Update db_setup.sql

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -4,5 +4,5 @@ GRANT ALL PRIVILEGES on bulgaria.* to 'web_user'@'%' IDENTIFIED BY 'Password1';
 FLUSH PRIVILEGES;
 use bulgaria;
 create table citizens (id int primary key auto_increment, citizen_type varchar(50), population int);
-INSERT INTO citizens (citizen_type, population) VALUES ('Мъже', 2416899);
-INSERT INTO citizens (citizen_type, population) VALUES ('Жени', 2626287);
+INSERT INTO citizens (citizen_type, population) VALUES ('Мъже', '2416899');
+INSERT INTO citizens (citizen_type, population) VALUES ('Жени', '2626287');
