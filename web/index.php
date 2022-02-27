3bec7ecf2db6ffc7650b6e7444f7cd26302a3f46
vvpanchev <vladislav_3@abv.bg>
2022-02-27 20:07:39 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,22 +5,15 @@
   </head>
   <body>
     <div align="center">
-      <h1>Факти за България</h1>
-      <img src="bulgaria-map.png" />
+      <h1>НАСЕЛЕНИЕ КЪМ 31.12. 2020 Г. ПО ПОЛ</h1>
+      <img src="https://www.nsi.bg/core/themes/bartik/images/BGR_NSI_Logo.png" />
       <table>
         <tr>
           <td>Площ</td>
           <td></td>
           <td>110 993.6 кв.км.</td>
         </tr>
-        <tr>
-          <td>Население</td>
-          <td></td>
-          <td>7 101 859</td>
-        </tr>
       </table>
-      <br />
-      <h1>Големи градове</h1>
       <table>
 <?php
 require_once ('config.php');
