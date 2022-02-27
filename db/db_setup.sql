d9d86868e155a345f6860c7093f5c0cfc20f11dd
vvpanchev <vladislav_3@abv.bg>
2022-02-27 16:02:02 +0200

    Update db_setup.sql

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -3,5 +3,5 @@ create database bulgaria character set utf8 collate utf8_general_ci;
 grant all on bulgaria.* to 'web_user'@'%' identified by 'Password1';
 use bulgaria;
 create table citizens (id int primary key auto_increment, citizen_type varchar(50), population int);
-INSERT INTO cities (citizen_type, population) VALUES ('Мъже', 2416899);
-INSERT INTO cities (citizen_type, population) VALUES ('Жени', 2626287);
+INSERT INTO citizens (citizen_type, population) VALUES ('Мъже', 2416899);
+INSERT INTO citizens (citizen_type, population) VALUES ('Жени', 2626287);
